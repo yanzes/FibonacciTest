41ba7f241379a03427b678c37e489e980f2d9ed6
yanzes caro <yanzes01@gmail.com>
2024-08-21 10:25:55 -0500

    Initial commit

---
+++ function fibonacci.js
@@ -0,0 +1,10 @@
+function fibonacci(num) {
+    const fib = [0,1]
+
+    for (let i = 2; i<= num; i++) {
+        fib [i] = fib[i-1] + fib [i-2]
+    }
+
+    return fib[num]
+}
+console.log(fibonacci(10));
